72035a257a3d6665b200a4e0fab90fd80ffe1ca7
Luis Paulo <devluis@icloud.com>
2023-09-25 20:22:39 -0300

    Adicionado novo projeto Alura viagens, curso 4 - aula 1 e 2

---
+++ Formações Alura/1) Domine a linguagem Swift/4) iOS - Auto layout com Constraints/AluraViagens/AluraViagens/HomeTableViewHeader.swift
@@ -0,0 +1,21 @@
+//
+//  HomeTableViewHeader.swift
+//  AluraViagens
+//
+//  Created by Luís Paulo Da Costa Cavalcante on 25/09/23.
+//
+
+import UIKit
+
+class HomeTableViewHeader: UIView {
+
+    @IBOutlet weak var tituloLabel: UILabel!
+    
+    
+    @IBOutlet weak var HeaderView: UIView!
+    
+    
+    @IBOutlet weak var BannerImageView: UIImageView!
+    
+
+}
---
+++ Formações Alura/1) Domine a linguagem Swift/4) iOS - Auto layout com Constraints/AluraViagens/AluraViagens/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  AluraViagens
+//
+//  Created by Luís Paulo Da Costa Cavalcante on 25/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var viagensTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viagensTableView.dataSource = self
+        viagensTableView.delegate = self
+        
+        view.backgroundColor = UIColor(red: 30/255, green: 59/255, blue: 119/255, alpha: 1)
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let headerView = Bundle.main.loadNibNamed("HomeTableViewHeader", owner: self, options: nil)?.first as? HomeTableViewHeader
+        
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 300 //retorno da altura
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        
+        cell.textLabel?.text = "viagem \(indexPath.row)"
+        
+        return cell
+    }
+}
