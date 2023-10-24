ba980dbad64f9ca899a4dcf51400c53026c83765
Luis Paulo <devluis@icloud.com>
2023-10-24 20:29:25 -0300

    adicionado aula 1 e 2 da formação 2

---
+++ Formações Alura/2) Construa aplicativos iOS com SwiftUI/1) iOS com SWIFTUI - construindo componentes e layouts/ChefDelivery/ChefDelivery/App/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  ChefDelivery
+//
+//  Created by Luís Paulo Da Costa Cavalcante on 24/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Criptomoedas")
+    }
+}
+
+#Preview {
+    ContentView()
+        .previewLayout(.sizeThatFits)
+}
