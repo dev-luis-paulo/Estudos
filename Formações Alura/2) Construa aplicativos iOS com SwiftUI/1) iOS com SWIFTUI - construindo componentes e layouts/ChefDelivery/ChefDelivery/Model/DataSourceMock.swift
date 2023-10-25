9576b2bf95d3806b5ea85efc5f9ce2eeedb85ff5
Luis Paulo <devluis@icloud.com>
2023-10-25 19:06:17 -0300

    adicionado aula 3

---
+++ Formações Alura/2) Construa aplicativos iOS com SwiftUI/1) iOS com SWIFTUI - construindo componentes e layouts/ChefDelivery/ChefDelivery/Model/DataSourceMock.swift
@@ -0,0 +1,18 @@
+//
+//  DataSourceMock.swift
+//  ChefDelivery
+//
+//  Created by Luís Paulo Da Costa Cavalcante on 25/10/23.
+//
+
+import Foundation
+
+let ordersMock: [OrderType] = [
+    OrderType(id: 1, name: "Restaurantes", image: "hamburguer"),
+    OrderType(id: 2, name: "Mercado", image: "mercado"),
+    OrderType(id: 3, name: "Farmácia", image: "farmácia"),
+    OrderType(id: 4, name: "Pet", image: "hpetshop"),
+    OrderType(id: 5, name: "Descontos", image: "descontos"),
+    OrderType(id: 6, name: "Bebidas", image: "bebidas"),
+    OrderType(id: 7, name: "Gourmet", image: "gourmet")
+]
