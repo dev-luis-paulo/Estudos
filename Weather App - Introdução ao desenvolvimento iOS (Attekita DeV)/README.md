905ba10448f2aaf9a0ad301e7ee59345cda78c60
Luis Paulo <devluis@icloud.com>
2023-10-31 15:14:59 -0300

    adicionado aula 1 e 2, readme

---
+++ Weather App - Introdução ao desenvolvimento iOS (Attekita DeV)/README.md
@@ -0,0 +1,24 @@
+# [Desenvolvendo um Aplicativo do Tempo - Curso de introdução ao desenvolvimento iOS)](https://www.youtube.com/watch?v=I8p8KlKEI_Y&list=PLprgbdnzrDkHjUr2mFq0ypg-v6RNChKA8)
+
+Esse curso feito por [Attekita Dev](https://www.youtube.com/@attekitadev) possui 7 aulas e ensina a desenvolver um aplicativo com informações do tempo. Durante meus estudos repliquei alguns códigos abordados em aula para adquirir conhecimento.
+
+## Exemplo de como acessar os arquivos
+
+O projeto deve ser executado através da IDE xcode e buildado através da tecla "Command + R"
+
+## Informações
+- Interface: UIKit (View Code)
+- Linguagem: Swift
+
+## Aulas
+
+### [Aula 1](https://www.youtube.com/watch?v=I8p8KlKEI_Y&list=PLprgbdnzrDkHjUr2mFq0ypg-v6RNChKA8)
+- Conhecedo a IDE Xcode
+- Criando nosso projeto do zero
+- Entendendo o Interface Builder
+- Criando os primeiros componentes UIKit
+
+### [Aula 2](https://www.youtube.com/watch?v=DOsN9unsL-0&list=PLprgbdnzrDkHjUr2mFq0ypg-v6RNChKA8&index=2)
+- Configurando o projeto sem storyboard
+- Introdução ao ViewCode
+- Autolayout
--- Weather App - Introdução ao desenvolvimento iOS (Attekita DeV)/Weather App/Weather App/ViewController.swift
+++ Weather App - Introdução ao desenvolvimento iOS (Attekita DeV)/Weather App/Weather App/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Só é inicializada quando for referenciada (Maior eficiencia, pois não está alocada na memória)
+    private lazy var customView: UIView = {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = .black
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupView()
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .red
+        
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        view.addSubview(customView)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            customView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            customView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
+            customView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
+            customView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100)
+        ])
+    }
 }
 
