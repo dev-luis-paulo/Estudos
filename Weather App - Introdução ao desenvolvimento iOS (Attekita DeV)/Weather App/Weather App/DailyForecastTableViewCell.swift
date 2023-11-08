44be85712ab286f831cfff660947074b24eae582
Luis Paulo <devluis@icloud.com>
2023-11-08 20:25:08 -0300

    adicionado aula 6

---
+++ Weather App - Introdução ao desenvolvimento iOS (Attekita DeV)/Weather App/Weather App/DailyForecastTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DailyForecastTableViewCell.swift
+//  Weather App
+//
+//  Created by Luís Paulo Da Costa Cavalcante on 08/11/23.
+//
+
+import UIKit
+
+class DailyForecastTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Weather App - Introdução ao desenvolvimento iOS (Attekita DeV)/Weather App/Weather App/UIView+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  UIView+Extensions.swift
+//  Weather App
+//
+//  Created by Luís Paulo Da Costa Cavalcante on 08/11/23.
+//
+
+import Foundation
