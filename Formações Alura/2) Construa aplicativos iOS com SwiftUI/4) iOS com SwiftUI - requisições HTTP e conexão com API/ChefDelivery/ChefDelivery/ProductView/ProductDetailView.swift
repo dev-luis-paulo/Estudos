bef6070e58250bd47310c7e2eabb47e4c01b9c35
Luis Paulo <devluis@icloud.com>
2024-01-30 23:51:29 -0300

    adicionado aula 4, verbo post

--- Formações Alura/2) Construa aplicativos iOS com SwiftUI/4) iOS com SwiftUI - requisições HTTP e conexão com API/ChefDelivery/ChefDelivery/ProductView/ProductDetailView.swift
+++ Formações Alura/2) Construa aplicativos iOS com SwiftUI/4) iOS com SwiftUI - requisições HTTP e conexão com API/ChefDelivery/ChefDelivery/ProductView/ProductDetailView.swift
@@ -12,6 +12,8 @@ struct ProductDetailView: View {
     let product: ProductType
     @State var productQuantity = 1
     
+    var service = HomeService()
+    
     var body: some View {
         
         ProductDetailHeaderView(product: product)
@@ -23,12 +25,14 @@ struct ProductDetailView: View {
         Spacer()
         
         Button {
-            print("Botao pressionado")
+            Task {
+                await confirmOrder()
+            }
         } label: {
             HStack{
                 Image(systemName: "cart")
                 
-                Text("Adicionar ao carrinho")
+                Text("Enviar pedido")
             }
             .padding(.horizontal, 32)
             .padding(.vertical, 16)
@@ -39,6 +43,21 @@ struct ProductDetailView: View {
             .cornerRadius(32)
             .shadow(color: Color("ColorRedDark").opacity(0.5), radius: 10)
         }
+    }
+    
+    func confirmOrder() async {
+        do {
+            let result = try await service.confirmOrder(product: product)
+            switch result {
+            case .success(let message):
+                print(message)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+        
     }
 }
 
