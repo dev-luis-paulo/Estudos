28eff7c113f53d6f712da476db0c0b5d921ab8ee
Luis Paulo <devluis@icloud.com>
2024-02-06 23:14:40 -0300

    Adicionado aula 2 - req get e async-await

--- Formações Alura/3) Evolua Apps em SwiftUI - CRUD, MVVM e Autenticação/1) iOS com SwiftUI - aplicações com CRUD e APIs REST/Voll Med/Vollmed/Services/WebService.swift
+++ Formações Alura/3) Evolua Apps em SwiftUI - CRUD, MVVM e Autenticação/1) iOS com SwiftUI - aplicações com CRUD e APIs REST/Voll Med/Vollmed/Services/WebService.swift
@@ -5,13 +5,51 @@
 //  Created by Giovanna Moeller on 12/09/23.
 //
 
-import Foundation
+import UIKit
 
 struct WebService {
     
     private let baseURL = "http://localhost:3000"
     
-    func getAllSpecialists() {
+    private let imageCache = NSCache<NSString, UIImage>()
+    
+    func downloadImage(from imageURL: String) async throws -> UIImage? {
+        guard let url = URL(string: imageURL) else {
+            print("Erro na url!")
+            return nil
+        }
         
+        //verifica cache
+        if let cachedImage = imageCache.object(forKey: imageURL as NSString) {
+            return cachedImage
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        guard let image = UIImage(data: data) else {
+            return nil
+        }
+        
+        //salva imagem em cache
+        imageCache.setObject(image, forKey: imageURL as NSString)
+        
+        return image
+    }
+    
+    func getAllSpecialists() async throws -> [Specialist]? {
+        let endpoint = baseURL + "/especialista"
+        
+        guard let url = URL(string: endpoint) else {
+            print("Erro na URL!")
+            return nil
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        
+        let specialists = try JSONDecoder().decode([Specialist].self, from: data)
+        
+        return specialists
     }
 }
+
+
+	
